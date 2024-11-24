9a988dd528c945ceee9c034b54befd11559328be
msasaki-jyobi <mao.sasaki@morijyobi.ac.jp>
2024-11-25 01:53:47 +0900

    Update CameraManager.cs

--- Scripts/Camera/CameraManager.cs
+++ Scripts/Camera/CameraManager.cs
@@ -68,27 +68,42 @@ namespace develop_easymovie
         /// 渡されたカメラに切り替える（アクティブカメラを切り替える）
         /// </summary>
         /// <param name="vcam"></param>
-        public async void ChangeActiveCamera(CinemachineVirtualCamera vcam, float blendTime = -1, Transform lookTarget = null, Transform followTarget = null)
+        public async void ChangeActiveCamera(CinemachineVirtualCamera vcam, float blendTime = -1, Transform lookTarget = null, Transform followTarget = null, Vector3 offset = default, bool freeCameraReset = true)
         {
             if (blendTime >= 0)
                 Brain.m_DefaultBlend.m_Time = blendTime;
 
             if (_activeVcam != null)
                 _activeVcam.m_Priority = 0;
-            foreach (var freeCam in FreeLooks)
-            {
-                if (freeCam == null) continue;
-                freeCam.m_Priority = 0;
-            }
+            if (freeCameraReset)
+                foreach (var freeCam in FreeLooks)
+                {
+                    if (freeCam == null) continue;
+                    freeCam.m_Priority = 0;
+                }
             foreach (var cam in VCams)
             {
-                if(cam == null) continue;
+                if (cam == null) continue;
                 cam.m_Priority = 0;
             }
             if (lookTarget != null)
                 vcam.m_LookAt = lookTarget;
             if (followTarget != null)
                 vcam.m_Follow = followTarget;
+            if (offset != default)
+            {
+                var x = Random.Range(-offset.x, offset.x);
+                var y = Random.Range(-offset.y, offset.y);
+                var z = Random.Range(-offset.z, offset.z);
+                var transposer = vcam.GetCinemachineComponent<CinemachineTransposer>();
+                if (transposer != null)
+                {
+                    transposer.m_FollowOffset.x = x;
+                    transposer.m_FollowOffset.y = y;
+                    transposer.m_FollowOffset.z = z;
+                }
+            }
+
 
             await UniTask.Delay(1);
 
@@ -133,13 +148,13 @@ namespace develop_easymovie
             if (cinemachineFreeLook == null)
                 return;
 
-                foreach (var cam in VCams)
+            foreach (var cam in VCams)
                 cam.m_Priority = 0;
             foreach (var freeCam in FreeLooks)
                 freeCam.m_Priority = 0;
 
             await UniTask.Delay(100);
-                cinemachineFreeLook.m_Priority = 30;
+            cinemachineFreeLook.m_Priority = 50;
         }
 
         ///// <summary>
@@ -199,7 +214,7 @@ namespace develop_easymovie
                 //if (CameraSlotNum.Value >= DefaultVcams.Count)
                 //    ChangeFreeLookCamera(DefaultFreeLook);
                 //else
-                    ChangeActiveCamera(DefaultVcams[0]);
+                ChangeActiveCamera(DefaultVcams[0]);
             }
         }
 
